06ac0b594982a095cc85b896ac8d04c2bf224775
Muhammad Abdullah Khabir <abdullah@abdullah.solutions>
2023-09-21 12:11:01 +0500

    configure ec2 with terraform and run a playbook with it

--- main.tf
+++ main.tf
@@ -11,4 +11,8 @@ resource "aws_instance" "myec2" {
   ami           = var.ami
   instance_type = var.ec2_type
   key_name      = aws_key_pair.mysshkey.key_name
+
+  provisioner "local-exec" {
+      command = "sleep 30 && ansible-playbook -i '${aws_instance.myec2.public_ip},' -e 'target_host=${aws_instance.myec2.public_ip}' -u admin ansible/configure-ec2.yml"
+  }
 }
